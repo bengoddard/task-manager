0c11779549fd4702cd049b8662de2429e2191487
Ben Goddard <goddard.ben04@gmail.com>
2026-01-20 18:20:13 -0700

    set up config.py

---
+++ server/config.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_jwt_extended import JWTManager
+
+app = Flask(__name__)
+
+app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "i-should-be-secret-and-stored-in-env-variables"
+
+jwt = JWTManager(app)
+
+app.json.compact = False
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt = Bcrypt(app)
+
+api = Api(app)
